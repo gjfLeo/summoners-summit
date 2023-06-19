1fda48a0467fa31bacb42fe8f75fddb50d5548c6
Liger <gjfLeo@yeah.net>
2023-06-19 15:29:22 +0800

    问题修复

--- components/game/GameRecordList.vue
+++ components/game/GameRecordList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grid mt items-center justify-center gap-4">
-    <template v-for="game in list" :key="game.id">
+    <template v-for="game in data" :key="game.id">
       <div class="grid items-center items-center gap-0" style="grid-template-columns: 8rem 4rem 8rem;">
         <div class="text-center text-xs grid-col-1/4">{{ game.tournamentName }}</div>
         <div class="text-center text-xs grid-col-1/4">{{ game.gameName }}</div>
@@ -10,12 +10,15 @@
         <div class="text-left">{{ game.playerB }}</div>
 
         <NuxtLink :to="`/deck/${game.deckA}`" class="flex items-center justify-end gap-1">
-          <TeamAvatars :team="getCharacterCardsInDeck(findDeck(game.deckA)!).join('-')" />
+          <TeamElements :team="game.teamA" />
+          <TeamAvatars :team="game.teamA" />
         </NuxtLink>
         <div class="text-center">VS</div>
-        <NuxtLink :to="`/deck/${game.deckA}`" class="flex items-center justify-start gap-1">
-          <TeamAvatars :team="getCharacterCardsInDeck(findDeck(game.deckA)!).join('-')" />
+        <NuxtLink :to="`/deck/${game.deckB}`" class="flex items-center justify-start gap-1">
+          <TeamAvatars :team="game.teamB" />
+          <TeamElements :team="game.teamB" />
         </NuxtLink>
+
         <div class="text-right text-sm">
           <span mr-2>{{ game.startWith === "A" ? "先手" : "后手" }}</span>
           <span v-if="game.winner === 'A'" text-orange-500>胜</span>
@@ -36,7 +39,19 @@
 import { findDeck } from "~/data";
 import type { Game } from "~/utils/types";
 
-defineProps<{
+const props = defineProps<{
   list: Game[];
 }>();
+
+const data = computed(() => props.list
+  .map((game) => {
+    const deckA = findDeck(game.deckA)!;
+    const deckB = findDeck(game.deckB)!;
+    return {
+      ...game,
+      teamA: getCharacterCardsInDeck(deckA).join("-"),
+      teamB: getCharacterCardsInDeck(deckB).join("-"),
+    };
+  }),
+);
 </script>
