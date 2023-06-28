4d2315c9861d0eddecb92535303d0f817c4be9b4
Liger <gjfLeo@yeah.net>
2023-06-28 13:58:58 +0800

    更新GitHub Actions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -83,7 +83,6 @@ export default defineNuxtConfig({
   },
 
   app: {
-    buildAssetsDir: process.env.NUXT_BUILD_ASSETS_DIR,
     head: {
       viewport: "width=device-width,initial-scale=1",
       link: [
