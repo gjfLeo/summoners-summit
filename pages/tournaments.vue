884ac36884c9fc9ff65968c53623cc3173f52847
Liger <gjfLeo@yeah.net>
2023-07-18 20:15:02 +0800

    选择游戏版本

--- pages/tournaments.vue
+++ pages/tournaments.vue
@@ -7,5 +7,11 @@ import { tournamentById } from "~/data";
 
 useHead({ title: "赛事 | 召唤之巅" });
 
-const list = Object.keys(tournamentById);
+const gameVersion = useGameVersion();
+
+const list = computed(() =>
+  Object.entries(tournamentById)
+    .filter(([_id, tournament]) => !gameVersion.value || tournament.gameVersion === gameVersion.value)
+    .map(([id]) => id),
+);
 </script>
