c603ef85c83f92dd7c0818e67a3ab595eed7b780
Liger <gjfLeo@yeah.net>
2023-07-28 10:23:48 +0800

    更新数据

--- data/tournaments/3.8/积分赛-共鸣杯1.ts
+++ data/tournaments/3.8/积分赛-共鸣杯1.ts
@@ -1,4 +1,4 @@
-import { Amber, AratakiItto, Barbara, Candace, Chongyun, FatuiPyroAgent, Fischl, KamisatoAyaka, Klee, KujouSara, Mona, Nahida, Ningguang, Noelle, RaidenShogun, Razor, RhodeiaOfLoch, SangonomiyaKokomi, Shenhe, YaeMiko } from "../../shared/character-cards";
+import { Amber, AratakiItto, Barbara, Bennett, Candace, Chongyun, ElectroHypostasis, FatuiPyroAgent, Fischl, KamisatoAyaka, Klee, KujouSara, Mona, Nahida, Ningguang, Noelle, RaidenShogun, Razor, RhodeiaOfLoch, SangonomiyaKokomi, Shenhe, Venti, YaeMiko, Yoimiya } from "../../shared/character-cards";
 import { defineTournament } from "../../shared/utils/tournaments";
 
 export default defineTournament({
@@ -8,7 +8,13 @@ export default defineTournament({
   stages: [
     {
       name: "16进8淘汰赛",
-      rules: ["BO3", "征服"],
+      rules: [
+        "BO3",
+        "征服",
+        ["禁用角色", "对阵双方各禁用一张角色牌"],
+        ["禁用阵容", "对阵双方各准备3套阵容，禁用对手一套阵容"],
+        ["可重复角色", "每名选手的3套阵容至少包含7张不同角色牌"],
+      ],
       parts: [
         {
           name: "DAY1",
@@ -17,7 +23,7 @@ export default defineTournament({
             {
               playerA: "曦雨",
               playerB: "郑明行",
-              video: "",
+              video: "https://www.bilibili.com/video/BV1uV411j7TD",
               games: [
                 {
                   playerACharacters: [Barbara, AratakiItto, Noelle],
@@ -36,7 +42,7 @@ export default defineTournament({
             {
               playerA: "浅冽",
               playerB: "雪国Ink",
-              video: "",
+              video: "https://www.bilibili.com/video/BV1uV411j7TD",
               games: [
                 {
                   playerACharacters: [Barbara, Noelle, AratakiItto],
@@ -55,7 +61,7 @@ export default defineTournament({
             {
               playerA: "铃紫",
               playerB: "诛仙还怡文",
-              video: "",
+              video: "https://www.bilibili.com/video/BV1uV411j7TD",
               games: [
                 {
                   playerACharacters: [Barbara, Ningguang, SangonomiyaKokomi],
@@ -74,7 +80,7 @@ export default defineTournament({
             {
               playerA: "式微",
               playerB: "音尘",
-              video: "",
+              video: "https://www.bilibili.com/video/BV1uV411j7TD",
               games: [
                 {
                   playerACharacters: [RaidenShogun, Candace, Razor],
@@ -98,7 +104,81 @@ export default defineTournament({
             },
           ],
         },
+        {
+          name: "DAY2",
+          date: "2023-07-26",
+          matches: [
+            {
+              playerA: "薄学",
+              playerB: "寂风",
+              video: "https://www.bilibili.com/video/BV1qM4y1p7MP",
+              games: [
+                {
+                  playerACharacters: [RhodeiaOfLoch, Fischl, FatuiPyroAgent],
+                  playerBCharacters: [RhodeiaOfLoch, YaeMiko, FatuiPyroAgent],
+                  starter: "B",
+                  winner: "A",
+                },
+                {
+                  playerACharacters: [Fischl, Venti, KamisatoAyaka],
+                  playerBCharacters: [RhodeiaOfLoch, YaeMiko, FatuiPyroAgent],
+                  starter: "B",
+                  winner: "A",
+                },
+              ],
+            },
+            {
+              playerA: "SY不歪",
+              playerB: "牛马牛牛马",
+              video: "https://www.bilibili.com/video/BV1qM4y1p7MP",
+              games: [
+                {
+                  playerACharacters: [RhodeiaOfLoch, FatuiPyroAgent, Fischl],
+                  playerBCharacters: [KamisatoAyaka, Yoimiya, Shenhe],
+                  starter: "A",
+                  winner: "B",
+                },
+                {
+                  playerACharacters: [RhodeiaOfLoch, FatuiPyroAgent, Fischl],
+                  playerBCharacters: [Chongyun, RaidenShogun, Shenhe],
+                  starter: "A",
+                  winner: "B",
+                },
+              ],
+            },
+            {
+              playerA: "自古二楼",
+              playerB: "pupa",
+              video: "https://www.bilibili.com/video/BV1qM4y1p7MP",
+              games: [
+                {
+                  playerACharacters: [FatuiPyroAgent, RhodeiaOfLoch, YaeMiko],
+                  playerBCharacters: [ElectroHypostasis, Bennett, Klee],
+                  starter: "B",
+                  winner: "B",
+                },
+                {
+                  playerACharacters: [FatuiPyroAgent, RhodeiaOfLoch, YaeMiko],
+                  playerBCharacters: [Chongyun, RaidenShogun, Shenhe],
+                  starter: "A",
+                  winner: "B",
+                },
+              ],
+            },
+          ],
+        },
       ],
     },
+    // {
+    //   name: "8进4淘汰赛",
+    //   rules: [
+    //     "BO5",
+    //     "征服",
+    //     ["禁用角色", "对阵双方各禁用一张角色牌"],
+    //     ["禁用阵容", "对阵双方各准备4套阵容，禁用对手一套阵容"],
+    //     ["可重复角色", "每名选手的4套阵容至少包含9张不同角色牌"],
+    //   ],
+    //   parts: [],
+    // },
   ],
 });
