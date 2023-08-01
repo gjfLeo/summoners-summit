631dcfec7f5d428ba0eeab8639e17fae540eb275
Liger <gjfleo@yeah.net>
2023-08-02 02:24:01 +0800

    feat: 更换 favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,24 @@ export default defineNuxtConfig({
     "@unocss/reset/tailwind-compat.css",
   ],
 
+  app: {
+    head: {
+      viewport: "width=device-width,initial-scale=1",
+      link: [
+        { rel: "icon", type: "image/svg+xml", href: `${process.env.NUXT_APP_BASE_URL ?? "/"}favicon.svg` },
+        { rel: "apple-touch-icon", href: `${process.env.NUXT_APP_BASE_URL ?? "/"}apple-touch-icon.png` },
+      ],
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { name: "description", content: "七圣召唤赛事资料" },
+        { name: "apple-mobile-web-app-status-bar-style", content: "black-translucent" },
+        // { "http-equiv": "Cache-Control", "content": "no-cache, no-store, must-revalidate" },
+        // { "http-equiv": "Pragma", "content": "no-cache" },
+        // { "http-equiv": "Expires", "content": "0" },
+      ],
+    },
+  },
+
   build: {
     transpile:
       process.env.NODE_ENV === "production"
@@ -35,6 +53,7 @@ export default defineNuxtConfig({
       },
     },
   },
+
   devtools: {
     enabled: false,
   },
