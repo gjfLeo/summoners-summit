07c7eee4e8d35b776c7b90bf9abc2b019e1c1708
Liger <gjfleo@yeah.net>
2023-08-03 01:47:59 +0800

    feat: 赛事列表支持切换版本

---
+++ pages/tournaments/[gameVersion].vue
@@ -0,0 +1,32 @@
+<template>
+  <template v-for="(list, version) in listByVersion" :key="version">
+    <NH2>{{ version }}</NH2>
+    <TournamentList :list="list" />
+  </template>
+</template>
+
+<script lang="ts" setup>
+import { NH2 } from "naive-ui";
+import Qs from "qs";
+import type { Tournament } from "~/utils/types";
+
+useHead({ title: "赛事 | 召唤之巅" });
+
+const { gameVersion } = useGameVersion({ detect: true });
+
+const { data } = await useFetch(`/api/tournaments?${Qs.stringify({ gameVersion: gameVersion.value })}`);
+if (!data.value) throw createError("获取数据失败");
+
+const { tournamentList } = data.value;
+
+const listByVersion = tournamentList.reduce<Record<string, Tournament[]>>(
+  (map, tournament) => {
+    const gameVersion = tournament.gameVersion;
+    const list = map[gameVersion] ?? [];
+    list.push(tournament as Tournament);
+    map[gameVersion] = list;
+    return map;
+  },
+  {},
+);
+</script>
--- pages/tournaments/index.vue
+++ pages/tournaments/index.vue
@@ -1,29 +1,11 @@
 <template>
-  <template v-for="(list, version) in listByVersion" :key="version">
-    <NH2>{{ version }}</NH2>
-    <TournamentList :list="list" />
-  </template>
+  <GameVersionNavigator />
 </template>
 
 <script lang="ts" setup>
-import { NH2 } from "naive-ui";
-import type { Tournament } from "~/utils/types";
-
-useHead({ title: "赛事 | 召唤之巅" });
-
-const { data } = await useFetch("/api/tournaments");
-if (!data.value) throw createError("获取数据失败");
-
-const { tournamentList } = data.value;
-
-const listByVersion = tournamentList.reduce<Record<string, Tournament[]>>(
-  (map, tournament) => {
-    const gameVersion = tournament.gameVersion;
-    const list = map[gameVersion] ?? [];
-    list.push(tournament as Tournament);
-    map[gameVersion] = list;
-    return map;
-  },
-  {},
-);
+definePageMeta({
+  middleware: [
+    "game-version-navigator",
+  ],
+});
 </script>
