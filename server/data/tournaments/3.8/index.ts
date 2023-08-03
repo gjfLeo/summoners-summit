8b85c85a2440772eb004a1166968ac5f6114626a
Liger <gjfLeo@yeah.net>
2023-08-03 18:05:13 +0800

    chore: 更新数据
    3.8寒天之钉杯16进8、8进4

--- server/data/tournaments/3.8/index.ts
+++ server/data/tournaments/3.8/index.ts
@@ -1,11 +1,13 @@
 import t11 from "./积分赛-月话杯";
 import t12 from "./积分赛-火星杯";
 import t13 from "./积分赛-共鸣杯1";
+import t14 from "./积分赛-寒天之钉杯";
 import t31 from "./积分赛试办赛-绯雪杯";
 
 export default [
   t11,
   t12,
   t13,
+  t14,
   t31,
 ];
