2ac90c5b9d35cc73d33b4f25a1a48cafcfda8e27
Liger <gjfLeo@yeah.net>
2023-08-03 12:54:19 +0800

    fix: 修复baseUrl不为空时的页面跳转

--- middleware/game-version-navigator.ts
+++ middleware/game-version-navigator.ts
@@ -2,10 +2,10 @@ export default defineNuxtRouteMiddleware((to, from) => {
   if (process.client) {
     const { gameVersionPath } = useGameVersion();
     if (to.name === from.name) {
-      return navigateTo(`${to.fullPath}/${gameVersionPath.value}`, { replace: true, open: { target: "_self" } });
+      return navigateTo(`${location.href}/${gameVersionPath.value}`, { replace: true, open: { target: "_self" } });
     }
     else {
-      return navigateTo(`${to.fullPath}/${gameVersionPath.value}`, { replace: true });
+      return navigateTo(`${to.path}/${gameVersionPath.value}`, { replace: true });
     }
   }
 });
