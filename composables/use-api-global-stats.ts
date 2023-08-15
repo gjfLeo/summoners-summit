b8bf34eb4854a752672e2e71adb938f393f9f5dc
Liger <gjfLeo@yeah.net>
2023-08-15 10:36:10 +0800

    feat: 首页显示数据统计

---
+++ composables/use-api-global-stats.ts
@@ -0,0 +1,5 @@
+export default async function useApiGlobelStats() {
+  const { data } = await useFetch("/api/v1/global-stats");
+  if (!data.value) throw createError("获取数据失败");
+  return data.value;
+}
---
+++ server/api/v1/global-stats/index.ts
@@ -0,0 +1,38 @@
+import type { R } from "~/utils/types";
+import { gameById } from "~/server/data";
+
+interface GlobalStats {
+  total: number;
+  totalWithDeck: number;
+  starterWin: number;
+}
+
+interface GlobalStatsData {
+  stats: Record<string, GlobalStats>;
+}
+
+const initStats = (): GlobalStats => ({ total: 0, totalWithDeck: 0, starterWin: 0 });
+
+export default defineEventHandler<R & GlobalStatsData>(() => {
+  const stats: Record<string, GlobalStats> = {};
+  Object.values(gameById).forEach((game) => {
+    const versionStats = stats[game.gameVersion] ?? (stats[game.gameVersion] = initStats());
+    versionStats.total++;
+    if (game.playerADeckId && game.playerBCharacters) {
+      versionStats.totalWithDeck++;
+    }
+    if (game.starter === game.winner) {
+      versionStats.starterWin++;
+    }
+  });
+  stats[""] = Object.values(stats).reduce(
+    (allStats, versionStats) => {
+      allStats.total += versionStats.total;
+      allStats.totalWithDeck += versionStats.totalWithDeck;
+      allStats.starterWin += versionStats.starterWin;
+      return allStats;
+    },
+    initStats(),
+  );
+  return { statusCode: 200, stats };
+});
