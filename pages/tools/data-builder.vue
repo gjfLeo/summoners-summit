e798fe2890dc637800804dac9e8a178fdbe4c698
Liger <gjfleo@yeah.net>
2023-08-25 02:08:12 +0800

    feat: 数据录入工具生成代码预览

--- pages/tools/data-builder.vue
+++ pages/tools/data-builder.vue
@@ -3,11 +3,22 @@
 
   <NHr />
 
-  <NCode :hljs="hljs" language="ts" :code="tournamentFormRef?.output" />
+  <NCard>
+    <NCollapse>
+      <NCollapseItem title="生成代码预览" name="1">
+        <NCode :hljs="hljs" language="ts" :code="code" />
+        <template #header-extra>
+          <NButton text @click.stop="copyCode">
+            <div class="i-carbon:copy" />
+          </NButton>
+        </template>
+      </NCollapseItem>
+    </NCollapse>
+  </NCard>
 </template>
 
 <script lang="ts" setup>
-import { NCode, NHr } from "naive-ui";
+import { NButton, NCard, NCode, NCollapse, NCollapseItem, NHr, useMessage } from "naive-ui";
 import hljs from "highlight.js/lib/core";
 import hljsTs from "highlight.js/lib/languages/typescript";
 import { EditorTournamentForm } from "#components";
@@ -15,4 +26,12 @@ import { EditorTournamentForm } from "#components";
 hljs.registerLanguage("ts", hljsTs);
 
 const tournamentFormRef = ref<InstanceType<typeof EditorTournamentForm>>();
+
+const code = computed(() => tournamentFormRef.value?.output ?? "");
+const { copy } = useClipboard({ source: code });
+const message = useMessage();
+async function copyCode() {
+  await copy();
+  message.success("已复制");
+}
 </script>
