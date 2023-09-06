1297bbb3e102cb198a66de10be3235f92c3a5678
Liger <gjfLeo@yeah.net>
2023-09-06 14:53:19 +0800

    feat: 添加卡牌图片检查页

---
+++ pages/tools/card-image-check.vue
@@ -0,0 +1,29 @@
+<template>
+  <div grid="~ cols-20 gap-1">
+    <template v-for="card in ALL_CHARACTER_CARDS" :key="card">
+      <div>
+        <CardAvatar :card="card" />
+      </div>
+    </template>
+  </div>
+  <div grid="~ cols-20 gap-1" mt>
+    <template v-for="card in ALL_CHARACTER_CARDS" :key="card">
+      <div>
+        <CardImage :card="card" />
+      </div>
+    </template>
+  </div>
+  <div grid="~ cols-20 gap-1" mt>
+    <template v-for="card in ALL_ACTION_CARDS" :key="card">
+      <div>
+        <CardImage :card="card" />
+      </div>
+    </template>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ALL_ACTION_CARDS, ALL_CHARACTER_CARDS } from "~/utils/types";
+
+useHead({ title: "卡牌图片检查页 - 召唤之巅" });
+</script>
