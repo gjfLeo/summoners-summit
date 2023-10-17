1c70628fbcf0846d9d7e4ca4fd9f2f50d175dc9c
Liger <gjfleo@yeah.net>
2023-10-18 01:31:50 +0800

    feat: 行动牌列表导出工具

---
+++ server/api/v1/action-cards/index.ts
@@ -0,0 +1,6 @@
+import { ALL_ACTION_CARDS } from "~/utils/types";
+import type { ActionCard, R } from "~/utils/types";
+
+export default defineEventHandler<R & { actionCards: ActionCard[] }>(() => {
+  return { statusCode: 200, actionCards: ALL_ACTION_CARDS };
+});
