0c664207edd854495f2b6b210b2f925dd270a228
Liger <gjfleo@yeah.net>
2023-10-22 22:42:05 +0800

    style: 玩家对应信息

--- server/data/players/index.ts
+++ server/data/players/index.ts
@@ -8,17 +8,17 @@ const playerNicknameMapRaw: Record<string, string> = {
   "RR夜莺": "LL起翼夜鹰",
   "RR抒情夜莺": "LL起翼夜鹰",
   "夜鹰": "LL起翼夜鹰",
-  
+
   "卡密sama42": "卡密sama",
 
   "原皮鸭": "皮皮鸭",
 
   "原来是筱梦": "岚落尘",
-  
+
   "洛甄黎": "八重妞妞",
 
-   "阿源今晚吃派蒙": "阿源",
-   "蓬莱寺九霄啊": "蓬莱寺九霄",
+  "阿源今晚吃派蒙": "阿源",
+  "蓬莱寺九霄啊": "蓬莱寺九霄",
   "幽羊": "一不小欣x",
   "月话牌社-音月": "月话音月",
   "梦一夕林": "萌萌哒的梦酱",
@@ -28,15 +28,15 @@ const playerNicknameMapRaw: Record<string, string> = {
   "本大爷还有仙跳": "道道不哭",
   "那菈琉環冰": "琉環冰",
   "齿轮": "齿轮mog",
-   "牌手Aurora": "Aurora",
-   "皮皮猪今晚吃阿源": "皮皮猪",
-   "七圣小铭同学": "小铭同学qaQ233",
+  "牌手Aurora": "Aurora",
+  "皮皮猪今晚吃阿源": "皮皮猪",
+  "七圣小铭同学": "小铭同学qaQ233",
   "Septorz": "Sept",
   "不足中行醉梦中": "今天不想吃苹果",
-   "沉湎丶": "桔梗",
+  "沉湎丶": "桔梗",
   "小宇cyy": "小宇",
   "魔法绵羊Tobby": "Tobby",
-   "xhope": "Xhope",
+  "xhope": "Xhope",
 };
 
 export const playerNicknameMap = Object.fromEntries(
