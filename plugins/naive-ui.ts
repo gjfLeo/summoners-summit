6a5d8c6028c161b72fab3dd6608b255a4b1ac446
Liger <gjfLeo@yeah.net>
2023-10-26 19:59:36 +0800

    fix: type

--- plugins/naive-ui.ts
+++ plugins/naive-ui.ts
@@ -16,7 +16,7 @@ export default defineNuxtPlugin((nuxtApp) => {
       }
       const originalMeta = originalRenderMeta();
       if ("then" in originalMeta) {
-        return originalMeta.then((resolvedOriginalMeta) => {
+        return originalMeta.then((resolvedOriginalMeta: any) => {
           return {
             ...resolvedOriginalMeta,
             headTags: resolvedOriginalMeta.headTags + collect(),
