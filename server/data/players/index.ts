7e5b2d6e71879487d54591ecc59c22b996140cbb
readice <101871184+readice@users.noreply.github.com>
2023-11-02 02:19:49 +0800

    Update index.ts

--- server/data/players/index.ts
+++ server/data/players/index.ts
@@ -8,15 +8,11 @@ const playerNicknameMapRaw: Record<string, string> = {
   "RR夜莺": "LL起翼夜鹰",
   "RR抒情夜莺": "LL起翼夜鹰",
   "夜鹰": "LL起翼夜鹰",
-
   "卡密sama42": "卡密sama",
-
   "原皮鸭": "皮皮鸭",
-
   "原来是筱梦": "岚落尘",
-
   "洛甄黎": "八重妞妞",
-
+  "妞妞嘛": "八重妞妞",
   "阿源今晚吃派蒙": "阿源",
   "蓬莱寺九霄啊": "蓬莱寺九霄",
   "幽羊": "一不小欣x",
@@ -36,7 +32,7 @@ const playerNicknameMapRaw: Record<string, string> = {
   "沉湎丶": "桔梗",
   "小宇cyy": "小宇",
   "魔法绵羊Tobby": "Tobby",
-  "xhope": "Xhope",
+  "xhope": "XHope",
   "pupa": "PUPA",
   "tonbo": "Tonbo",
   "蛋黄汐夕": "水果鲨啦",
@@ -45,6 +41,10 @@ const playerNicknameMapRaw: Record<string, string> = {
   "「亦然」": "亦然",
   "zjyang": "宇宙蓝色闪光",
   "今天不想吃苹果_": "今天不想吃苹果",
+  "一不小欣x": "一不小欣z",
+  "冰糖蕊梨": "蕊梨",
+  "干瘪的小果核": "小果核",
+  
 
   "甜甜花酿鸡队": "",
   "莲花酥队": "",
