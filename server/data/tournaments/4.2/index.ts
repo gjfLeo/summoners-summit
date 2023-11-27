8892dc81939d9c5a3f8a90b53e7484dad5c249a3
Liger <gjfleo@yeah.net>
2023-11-27 17:29:49 +0800

    chore: 更新数据
    closes #79, closes #80, closes #81

---
+++ server/data/tournaments/4.2/index.ts
@@ -0,0 +1,5 @@
+import t11 from "./积分赛-寒天之钉杯";
+
+export default [
+  t11,
+];
--- server/data/tournaments/index.ts
+++ server/data/tournaments/index.ts
@@ -2,8 +2,10 @@ import t38 from "./3.8";
 import t37 from "./3.7";
 import t40 from "./4.0";
 import t41 from "./4.1";
+import t42 from "./4.2";
 
 export default [
+  ...t42,
   ...t41,
   ...t40,
   ...t38,
