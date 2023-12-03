7174a0f0180e115de109533962d939f2387a9640
readice <101871184+readice@users.noreply.github.com>
2023-12-04 00:35:37 +0800

    Update index.ts

--- server/data/tournaments/4.2/index.ts
+++ server/data/tournaments/4.2/index.ts
@@ -1,11 +1,11 @@
 import t11 from "./积分赛-寒天之钉杯";
 import t12 from "./积分赛-共鸣杯";
 import t13 from "./积分赛-好游快爆杯";
-import t14 from "./主播赛-虎牙";
+
 
 export default [
   t11,
   t12,
   t13,
-  t14,
+
 ];
