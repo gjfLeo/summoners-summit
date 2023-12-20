b38907365a81ac5033727c6e5276f1ddcd619f22
readice <101871184+readice@users.noreply.github.com>
2023-12-20 11:28:06 +0800

    Update index.ts

--- server/data/tournaments/index.ts
+++ server/data/tournaments/index.ts
@@ -3,8 +3,10 @@ import t37 from "./3.7";
 import t40 from "./4.0";
 import t41 from "./4.1";
 import t42 from "./4.2";
+import t43 from "./4.3";
 
 export default [
+  ...t43,
   ...t42,
   ...t41,
   ...t40,
