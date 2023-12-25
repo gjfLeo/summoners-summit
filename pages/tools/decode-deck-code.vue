3e5f54e9382255d24e1d77504ff1b143408508f5
gjfLeo <gjfLeo@yeah.net>
2023-12-25 20:10:10 +0800

    feat: 牌组分享码解析页面

--- pages/tools/decode-deck-code.vue
+++ pages/tools/decode-deck-code.vue
@@ -1,7 +1,7 @@
 <template>
   <NInputGroup>
-    <NInput v-model:value="shareCode" class="font-mono" />
-    <NButton @click="resultDeck = decode()">搜索</NButton>
+    <NInput v-model:value="shareCode" placeholder="请输入牌组分享码" />
+    <NButton @click="resultDeck = decode()">解析</NButton>
   </NInputGroup>
   <template v-if="resultDeck">
     <!-- 角色牌 -->
@@ -17,14 +17,15 @@
       </template>
     </div>
   </template>
-  <pre>{{ resultDeck }}</pre>
 </template>
 
 <script lang="ts" setup>
 import { decodeDeckCode } from "~/utils/decks";
 import type { ActionCard, Deck } from "~/utils/types";
 
-useHead({ title: "牌组分享码解码" });
+useHead({ title: "牌组分享码解析" });
+
+const message = useMessage();
 
 const shareCode = ref("AxBQ9BAPA0Aw9UwPBGDA9rwPC5DA+cEPDMCQ/MkPDdDg/eEQDqEQC+IQDrEgDvEQD+AA");
 const resultDeck = ref<Pick<Deck, "characterCards" | "actionCards">>();
@@ -34,6 +35,12 @@ const actionCards = computed(() => Object.entries(resultDeck.value?.actionCards 
   .flatMap(([card, count]) => Array.from({ length: count as number }, () => card as ActionCard)));
 
 function decode() {
-  return decodeDeckCode(shareCode.value);
+  try {
+    return decodeDeckCode(shareCode.value);
+  }
+  catch {
+    message.error("解析失败");
+    return undefined;
+  }
 }
 </script>
