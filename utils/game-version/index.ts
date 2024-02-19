fbcede388a712b5820f2b04648dcbf7d85448ad1
readice <101871184+readice@users.noreply.github.com>
2024-02-19 20:51:28 +0800

    Update index.ts

--- utils/game-version/index.ts
+++ utils/game-version/index.ts
@@ -18,7 +18,7 @@ const ALL_GAME_VERSIONS_INFO = {
   "4.1": defineGameVersionInfo({ gameVersion: "4.1", season: 2 }),
   "4.2": defineGameVersionInfo({ gameVersion: "4.2", season: 2 }),
   "4.3": defineGameVersionInfo({ gameVersion: "4.3", season: 2 }),
-  // "4.4": defineGameVersionInfo({ gameVersion: "4.4", season: 2 }),
+  "4.4": defineGameVersionInfo({ gameVersion: "4.4", season: 2 }),
 };
 export type GameVersion = keyof typeof ALL_GAME_VERSIONS_INFO;
 export type GameVersionOptional = GameVersion | "";
