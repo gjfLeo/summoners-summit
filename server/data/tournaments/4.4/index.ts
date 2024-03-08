e2ab31388f98fecb57e909cb12918921614d3511
readice <101871184+readice@users.noreply.github.com>
2024-03-08 08:47:13 +0800

    Update index.ts

--- server/data/tournaments/4.4/index.ts
+++ server/data/tournaments/4.4/index.ts
@@ -3,6 +3,9 @@ import t12 from "./积分赛-共鸣杯";
 import t13 from "./其他-微博杯";
 import t14 from "./积分赛-月话杯";
 import t15 from "./积分赛-火星杯";
+import t16 from "./积分赛-好游快爆杯";
+import t17 from "./积分赛-米游社";
+
 
 export default [
   t11,
@@ -10,4 +13,6 @@ export default [
   t13,
   t14,
   t15,
+  t16,
+  t17,
 ];
