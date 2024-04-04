e6a07ee4e9a3c2dd21b4a4dd0705a7ee418d34d7
readice <101871184+readice@users.noreply.github.com>
2024-04-05 06:59:38 +0800

    Update 积分赛-共鸣杯.ts

--- server/data/tournaments/4.5/积分赛-共鸣杯.ts
+++ server/data/tournaments/4.5/积分赛-共鸣杯.ts
@@ -335,6 +335,86 @@ export default defineTournament({
             },
           ],
         },
+        {
+          name: "DAY2",
+          date: "2024-04-03",
+          matches: [
+            {
+              playerA: "雪国Ink",
+              playerB: "Dr.666",
+              video: "https://www.bilibili.com/video/BV1Lj421R7gE",
+              banned: [
+                {
+                  playerACharacters: ["莫娜", "雷音权现", "「女士」"],
+                  playerBCharacters: ["林尼", "千年珍珠骏麟", "若陀龙王"],
+                },
+              ],
+              games: [
+                {
+                  playerACharacters: ["林尼", "千年珍珠骏麟", "若陀龙王"],
+                  playerBCharacters: ["夜兰", "雷音权现", "「女士」"],
+                  starter: "B",
+                  winner: "A",
+                },
+                {
+                  playerACharacters: ["行秋", "重云", "雷泽"],
+                  playerBCharacters: ["夜兰", "雷音权现", "「女士」"],
+                  starter: "A",
+                  winner: "A",
+                },
+                {
+                  playerACharacters: ["莱依拉", "芭芭拉", "那维莱特"],
+                  playerBCharacters: ["愚人众·雷萤术士", "莫娜", "纳西妲"],
+                  starter: "A",
+                  winner: "B",
+                },
+                {
+                  playerACharacters: ["莱依拉", "芭芭拉", "那维莱特"],
+                  playerBCharacters: ["诺艾尔", "迪希雅", "五郎"],
+                  starter: "A",
+                  winner: "A",
+                },
+              ],
+            },
+            {
+              playerA: "弦聆aeon",
+              playerB: "冰红茶心甘晴愿",
+              video: "https://www.bilibili.com/video/BV1Nz421Z7Fn",
+              banned: [
+                {
+                  playerACharacters: ["莫娜", "枫原万叶", "「女士」"],
+                  playerBCharacters: ["莱依拉", "那维莱特", "若陀龙王"],
+                },
+              ],
+              games: [
+                {
+                  playerACharacters: ["林尼", "千年珍珠骏麟", "若陀龙王"],
+                  playerBCharacters: ["林尼", "千年珍珠骏麟", "纳西妲"],
+                  starter: "B",
+                  winner: "A",
+                },
+                {
+                  playerACharacters: ["行秋", "重云", "雷泽"],
+                  playerBCharacters: ["林尼", "千年珍珠骏麟", "纳西妲"],
+                  starter: "A",
+                  winner: "A",
+                },
+                {
+                  playerACharacters: ["夜兰", "雷音权现", "无相之冰"],
+                  playerBCharacters: ["林尼", "千年珍珠骏麟", "纳西妲"],
+                  starter: "B",
+                  winner: "B",
+                },
+                {
+                  playerACharacters: ["夜兰", "雷音权现", "无相之冰"],
+                  playerBCharacters: ["莫娜", "「女士」", "雷音权现"],
+                  starter: "A",
+                  winner: "A",
+                },
+              ],
+            },
+          ],
+        },
       ],
     },
   ],
