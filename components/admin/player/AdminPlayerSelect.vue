6f90331a87f710a067e6b02cb064860afa48f6db
Liger <gjfleo@yeah.net>
2024-04-19 01:06:07 +0800

    commit

--- components/admin/player/AdminPlayerSelect.vue
+++ components/admin/player/AdminPlayerSelect.vue
@@ -1,14 +1,16 @@
-<script lang="ts" setup>
+<script lang="tsx" setup>
 import Pinyin from "pinyin-match";
+import type { SelectRenderLabel, SelectRenderTag } from "naive-ui";
 import type { Player } from "~/types/data";
+import { NText } from "#components";
 
 const props = defineProps<{
   players: Player[];
-  value: string;
+  value: string | null;
 }>();
 
 const emit = defineEmits<{
-  (e: "update:value", value: string): void;
+  (e: "update:value", value: string | null): void;
 }>();
 
 const playerId = useVModel(props, "value", emit);
@@ -28,6 +30,31 @@ const selectFilter: SelectFilter = (pattern, option) => {
   return player.id === pattern || player.uids.includes(pattern)
     || [player.uniqueName, ...player.aliases].some(name => Pinyin.match(name, pattern));
 };
+
+const renderLabel: SelectRenderLabel = (option) => {
+  const player = option.player as Player;
+  if (!player) return <></>;
+  return (
+    <div class="flex flex-col p-block-2">
+      <div class="flex items-center gap-2">
+        <div>{player.uniqueName}</div>
+        <NText class="text-xs" depth={3}>{player.uids}</NText>
+      </div>
+      <NText class="text-xs" depth={3}>{player.aliases.join("\u2006/\u2006")}</NText>
+    </div>
+  );
+};
+
+const renderTag: SelectRenderTag = ({ option }) => {
+  const player = option.player as Player;
+  if (!player) return <></>;
+  return (
+    <div class="flex items-center gap-2">
+      <div>{player.uniqueName}</div>
+      <div class="text-xs opacity-50">{player.uids}</div>
+    </div>
+  );
+};
 </script>
 
 <template>
@@ -35,6 +62,9 @@ const selectFilter: SelectFilter = (pattern, option) => {
     v-model:value="playerId"
     :options="options"
     filterable
+    clearable
     :filter="selectFilter"
+    :render-tag="renderTag"
+    :render-label="renderLabel"
   />
 </template>
