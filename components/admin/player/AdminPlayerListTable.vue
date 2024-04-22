7e211e5d19bc579dfbcc481c593400ce6100182a
Liger <gjfleo@yeah.net>
2024-04-22 22:19:17 +0800

    commit

---
+++ components/admin/player/AdminPlayerListTable.vue
@@ -0,0 +1,79 @@
+<script lang="tsx" setup>
+import { NButton, NTag } from "#components";
+import type { Player } from "~/types/data";
+
+defineProps<{
+  data: Player[];
+  loading: boolean;
+}>();
+
+const emit = defineEmits<{
+  (e: "selectUniqueName", id: Player["id"]): void;
+  (e: "mergePlayerData", id: Player["id"]): void;
+}>();
+
+const { t } = useI18n();
+
+const columns: DataTableColumn<Player>[] = [
+  {
+    key: "id",
+    title: "ID",
+    width: "8rem",
+    className: "font-mono text-xs",
+  },
+  {
+    key: "uids",
+    title: "UID",
+    width: "6rem",
+    className: "font-mono text-xs",
+    render: (row) => {
+      return (
+        <div class="flex flex-col">
+          {row.uids.map(uid => <div>{uid}</div>)}
+        </div>
+      );
+    },
+  },
+  {
+    key: "uniqueName",
+    title: t("player.mainNickname"),
+    width: "18rem",
+  },
+  {
+    key: "aliases",
+    title: t("player.otherNicknames"),
+    render: (row) => {
+      return (
+        <div class="flex flex-wrap gap-2">
+          {row.aliases?.map(alias => <NTag>{alias}</NTag>)}
+        </div>
+      );
+    },
+  },
+  {
+    key: "actions",
+    width: "8rem",
+    render: (row) => {
+      return (
+        <div class="flex flex-wrap gap-2">
+          <NButton text focusable={false} onClick={() => emit("selectUniqueName", row.id)}>
+            <div class="i-carbon:edit" />
+          </NButton>
+          <NButton text focusable={false} onClick={() => emit("mergePlayerData", row.id)}>
+            <div class="i-carbon:direction-merge" />
+          </NButton>
+        </div>
+      );
+    },
+  },
+];
+</script>
+
+<template>
+  <NDataTable
+    :data="data"
+    :columns="columns"
+    :loading="loading"
+    flex-height
+  />
+</template>
--- pages/admin/players/index.vue
+++ pages/admin/players/index.vue
@@ -7,8 +7,6 @@ definePageMeta({ title: "site.titles.admin.players" });
 const playerUniqueNameDialog = ref<InstanceType<typeof AdminPlayerUniqueNameDialog>>();
 const playerMergeDialog = ref<InstanceType<typeof AdminPlayerMergeDialog>>();
 
-const { t } = useI18n();
-
 const { data, pending, refresh } = await useFetch("/api/v3/players/list");
 
 const players = computed(() => data.value?.players ?? []);
@@ -29,65 +27,6 @@ const filteredPlayers = computed(() => {
     return false;
   });
 });
-
-const columns: DataTableColumn<typeof players.value[number]>[] = [
-  {
-    key: "id",
-    title: "ID",
-    width: "8rem",
-    className: "font-mono text-xs",
-  },
-  {
-    key: "uids",
-    title: "UID",
-    width: "6rem",
-    className: "font-mono text-xs",
-    render: (row) => {
-      return <div class="flex flex-col">{row.uids.map(uid => <div>{uid}</div>)}</div>;
-    },
-  },
-  {
-    key: "uniqueName",
-    title: t("player.mainNickname"),
-    width: "18rem",
-  },
-  {
-    key: "aliases",
-    title: t("player.otherNicknames"),
-    render: (row) => {
-      return h(
-        "div",
-        { class: "flex flex-wrap gap-2" },
-        (row.aliases ?? []).map(alias => h(
-          NTag,
-          () => alias,
-        )),
-      );
-    },
-  },
-  {
-    key: "actions",
-    width: "8rem",
-    render: (row) => {
-      return h(
-        "div",
-        { class: "flex flex-wrap gap-2" },
-        [
-          h(
-            NButton,
-            { text: true, focusable: false, onClick: () => playerUniqueNameDialog.value?.show(row.id) },
-            () => h("div", { class: "i-carbon:edit" }),
-          ),
-          h(
-            NButton,
-            { text: true, focusable: false, onClick: () => playerMergeDialog.value?.show(row.id, players.value) },
-            () => h("div", { class: "i-carbon:direction-merge" }),
-          ),
-        ],
-      );
-    },
-  },
-];
 </script>
 
 <template>
@@ -96,12 +35,12 @@ const columns: DataTableColumn<typeof players.value[number]>[] = [
       <div><NInput v-model:value="filterText" clearable /></div>
       <NButton type="primary" secondary @click="refresh()"><div class="i-carbon:renew" /></NButton>
     </div>
-    <NDataTable
+    <AdminPlayerListTable
       :data="filteredPlayers"
-      :columns="columns"
       :loading="pending"
-      flex-height
       class="flex-table"
+      @select-unique-name="playerUniqueNameDialog?.show($event)"
+      @merge-player-data="playerMergeDialog?.show($event, players)"
     />
     <div v-show="false">
       <AdminPlayerUniqueNameDialog ref="playerUniqueNameDialog" @done="refresh()" />
