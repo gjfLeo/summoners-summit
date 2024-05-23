bbaf627c29d2b98c42044652f340214eb5990457
readice <101871184+readice@users.noreply.github.com>
2024-05-24 00:14:45 +0800

    Update 积分赛-好游快爆杯.ts

--- server/data/tournaments/4.6/积分赛-好游快爆杯.ts
+++ server/data/tournaments/4.6/积分赛-好游快爆杯.ts
@@ -146,5 +146,88 @@ export default defineTournament({
         },
       ],
     },
+    {
+      name: "半决赛",
+      rules: {
+        numDecks: 3,
+        numDecksBanned: 1,
+        numCharactersBanned: 11,
+        numCharactersRequired: 7,
+        numGames: 3,
+        mode: "Conquest",
+      },
+      parts: [
+        {
+          name: "",
+          date: "2024-05-23",
+          matches: [
+            {
+              playerA: "空寻的小迷妹",
+              playerB: "木瓜豆腐",
+              video: "https://live.i3839.com/n/hykb/zhibo/m/?comm_id=14&imm=0&gameid=106235",
+              banned: [
+                {
+                  playerACharacters: ["重云"],
+                  playerBCharacters: ["那维莱特"],
+                },
+                {
+                  playerACharacters: ["莱依拉", "若陀龙王", "深渊使徒·激流"],
+                  playerBCharacters: ["林尼", "若陀龙王", "深渊使徒·激流"],
+                },
+              ],
+              games: [
+                {
+                  playerACharacters: ["莫娜", "雷音权现", "优菈"],
+                  playerBCharacters: ["千年珍珠骏麟", "深渊使徒·激流", "班尼特"],
+                  starter: "B",
+                  winner: "B",
+                },
+                {
+                  playerACharacters: ["莫娜", "雷音权现", "优菈"],
+                  playerBCharacters: ["久岐忍", "深渊使徒·激流", "绮良良"],
+                  starter: "B",
+                  winner: "A",
+                },
+                {
+                  playerACharacters: ["莫娜", "雷音权现", "「女士」"],
+                  playerBCharacters: ["久岐忍", "深渊使徒·激流", "绮良良"],
+                  starter: "A",
+                  winner: "B",
+                },
+              ],
+            },
+            {
+              playerA: "祁音",
+              playerB: "人子歌于途",
+              video: "https://live.i3839.com/n/hykb/zhibo/m/?comm_id=14&imm=0&gameid=106235",
+              banned: [
+                {
+                  playerACharacters: ["重云"],
+                  playerBCharacters: ["那维莱特"],
+                },
+                {
+                  playerACharacters: ["莱依拉", "行秋", "芭芭拉"],
+                  playerBCharacters: ["久岐忍", "若陀龙王", "那维莱特"],
+                },
+              ],
+              games: [
+                {
+                  playerACharacters: ["久岐忍", "绮良良", "深渊使徒·激流"],
+                  playerBCharacters: ["莱依拉", "深渊使徒·激流", "若陀龙王"],
+                  starter: "A",
+                  winner: "A",
+                },
+                {
+                  playerACharacters: ["久岐忍", "深渊使徒·激流", "若陀龙王"],
+                  playerBCharacters: ["莫娜", "早柚", "优菈"],
+                  starter: "A",
+                  winner: "A",
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
   ],
 });
