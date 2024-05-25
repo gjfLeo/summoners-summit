d4fd8d2d98b0421ccc906ac47be0f79fe8bd6dd2
Trần Đặng Gia Bảo <37262651+TwoQuantumBits@users.noreply.github.com>
2024-05-25 17:19:48 +0000

    Update index.ts

--- server/data/tournaments/4.6/index.ts
+++ server/data/tournaments/4.6/index.ts
@@ -1,9 +1,11 @@
 import t11 from "./积分赛-共鸣杯";
 import t12 from "./积分赛-寒天之钉杯";
 import t13 from "./积分赛-好游快爆杯";
+import t41 from "./外服赛事-小王子杯-大西洋赛区-常规赛";
 
 export default [
   t11,
   t12,
   t13,
+  t41,
   ];
