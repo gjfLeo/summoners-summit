4c8acfb6b4bf06ef28a308aa22028ecd5018d99b
readice <101871184+readice@users.noreply.github.com>
2024-05-31 06:17:03 +0800

    Update index.ts

--- server/data/tournaments/4.6/index.ts
+++ server/data/tournaments/4.6/index.ts
@@ -1,9 +1,13 @@
 import t11 from "./积分赛-共鸣杯";
 import t12 from "./积分赛-寒天之钉杯";
 import t13 from "./积分赛-好游快爆杯";
+import t14 from "./积分赛-月话杯";
+import t15 from "./积分赛-火星杯";
 
 export default [
   t11,
   t12,
   t13,
+  t14,
+  t15,
   ];
