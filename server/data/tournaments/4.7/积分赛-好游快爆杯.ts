764832f571c7c12b5ea0bec7628272a586914101
readice <101871184+readice@users.noreply.github.com>
2024-07-04 00:53:09 +0800

    Update 积分赛-好游快爆杯.ts

--- server/data/tournaments/4.7/积分赛-好游快爆杯.ts
+++ server/data/tournaments/4.7/积分赛-好游快爆杯.ts
@@ -152,5 +152,88 @@ export default defineTournament({
         },
       ],
     },
+    {
+      name: "半决赛",
+      rules: {
+        numDecks: 3,
+        numDecksBanned: 1,
+        numCharactersBanned: 1,
+        numCharactersRequired: 7,
+        numGames: 3,
+        mode: "Conquest",
+      },
+      parts: [
+        {
+          name: "",
+          date: "2024-07-03",
+          matches: [
+            {
+              playerA: "胡桃花开青雀鸣",
+              playerB: "majikohanatan",
+              video: "https://live.i3839.com/n/hykb/zhibo/m/?comm_id=14&imm=0&gameid=106235",
+              banned: [
+                {
+                  playerACharacters: ["绮良良"],
+                  playerBCharacters: ["吞星之鲸"],
+                },
+                {
+                  playerACharacters: ["林尼", "久岐忍", "瑶瑶"],
+                  playerBCharacters: ["那维莱特", "若陀龙王", "莱依拉"],
+                },
+              ],
+              games: [
+                {
+                  playerACharacters: ["久岐忍", "瑶瑶", "芙宁娜"],
+                  playerBCharacters: ["久岐忍", "瑶瑶", "深渊使徒·激流"],
+                  starter: "A",
+                  winner: "B",
+                },
+                {
+                  playerACharacters: ["圣骸飞蛇", "圣骸毒蝎", "阿佩普的绿洲守望者"],
+                  playerBCharacters: ["芙宁娜", "深渊使徒·激流", "莱欧斯利"],
+                  starter: "A",
+                  winner: "A",
+                },
+                {
+                  playerACharacters: ["久岐忍", "瑶瑶", "芙宁娜"],
+                  playerBCharacters: ["芙宁娜", "深渊使徒·激流", "莱欧斯利"],
+                  starter: "B",
+                  winner: "A",
+                },
+              ],
+            },
+            {
+              playerA: "吟风咏月",
+              playerB: "爱酱单推人",
+              video: "https://live.i3839.com/n/hykb/zhibo/m/?comm_id=14&imm=0&gameid=106235",
+              banned: [
+                {
+                  playerACharacters: ["圣骸飞蛇"],
+                  playerBCharacters: ["吞星之鲸"],
+                },
+                {
+                  playerACharacters: ["深渊使徒·激流", "早柚", "重云"],
+                  playerBCharacters: ["深渊使徒·激流", "早柚", "重云"],
+                },
+              ],
+              games: [
+                {
+                  playerACharacters: ["芙宁娜", "莱欧斯利", "深渊使徒·激流"],
+                  playerBCharacters: ["芙宁娜", "深渊使徒·激流", "莱欧斯利"],
+                  starter: "B",
+                  winner: "A",
+                },
+                {
+                  playerACharacters: ["那维莱特", "若陀龙王", "莱依拉"],
+                  playerBCharacters: ["若陀龙王", "无相之冰", "那维莱特"],
+                  starter: "A",
+                  winner: "A",
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
   ],
 });
