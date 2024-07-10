7f7490093ff135be41624ea9532aa676c9f4280e
Liger <gjfleo@yeah.net>
2024-07-11 01:04:17 +0800

    commit

--- components/site/SiteHeaderGameVersionSelect.vue
+++ components/site/SiteHeaderGameVersionSelect.vue
@@ -1,7 +1,28 @@
 <template>
-  <GameVersionSelect v-model:value="gameVersion" />
+  <GameVersionSelect
+    v-model:value="gameVersion"
+    :clearable="false"
+    class="w-6.25rem"
+    @update:value="updateGameVersion"
+  />
 </template>
 
 <script lang="ts" setup>
+import type { GameVersionId } from "~/types";
+
 const { gameVersion } = useGameVersion();
+
+const route = useRoute();
+
+function updateGameVersion(gameVersion: GameVersionId) {
+  if ("gameVersion" in route.params && typeof route.params.gameVersion === "string") {
+    return navigateTo({
+      name: route.name,
+      params: {
+        ...route.params,
+        gameVersion: getGameVersionPath(gameVersion),
+      },
+    } as any);
+  }
+}
 </script>
