6bd61a6664020523fd8472e43e8db51fc07be19a
gjfLeo <gjfLeo@yeah.net>
2024-07-26 14:28:43 +0800

    commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,7 +29,7 @@ export default defineNuxtConfig({
     // when using generate, payload js assets included in sw precache manifest
     // but missing on offline, disabling extraction it until fixed
     // payloadExtraction: false,
-    renderJsonPayloads: true,
+    // renderJsonPayloads: true,
     typedPages: true,
   },
 
