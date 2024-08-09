a33dfdbcc90705ab7fbe6bb1efae19d58a7b4c38
gjfLeo <gjfLeo@yeah.net>
2024-08-09 19:34:48 +0800

    feat: 对局列表分页修改

--- components/game/GameList.vue
+++ components/game/GameList.vue
@@ -1,19 +1,29 @@
 <template>
-  <div
-    ref="dom"
-    un-grid="~ justify-evenly gap-4"
-    :style="{ gridTemplateColumns: `repeat(${columns}, 1fr)` }"
-  >
-    <template v-for="game in list" :key="`${game.id}-${game.winner}`">
-      <GameListItem :game="game" />
-    </template>
-  </div>
-  <div class="mt" un-flex="~ justify-end">
-    <NPagination
-      v-model:page="page"
-      :item-count="games.length"
-      :page-size="pageSize"
-    />
+  <div>
+    <div
+      ref="listDom"
+      un-grid="~ justify-evenly gap-4"
+      :style="{ gridTemplateColumns: `repeat(${columns}, 1fr)` }"
+    >
+      <template v-for="game in list" :key="`${game.id}-${game.winner}`">
+        <GameListItem :game="game" />
+      </template>
+    </div>
+    <div class="mt <md:hidden" un-flex="~ justify-end">
+      <NPagination
+        v-model:page="page"
+        :item-count="games.length"
+        :page-size="pageSize"
+      />
+    </div>
+    <div class="mt md:hidden" un-flex="~ justify-end">
+      <NPagination
+        v-model:page="page"
+        :item-count="games.length"
+        :page-size="pageSize"
+        simple
+      />
+    </div>
   </div>
 </template>
 
@@ -24,11 +34,11 @@ const props = defineProps<{
   games: GameDetail[];
 }>();
 
-const dom = ref<HTMLDivElement>();
-const { width } = useElementSize(dom);
-const columns = computed(() => Math.floor(width.value / remToPx(22)));
+const listDom = ref<HTMLDivElement>();
+const { width } = useElementSize(listDom);
+const columns = computed(() => Math.max(Math.floor(width.value / remToPx(22)), 1));
 
-const pageSize = computed(() => Math.max(columns.value * 2, 1));
+const pageSize = computed(() => columns.value * 2);
 const page = ref(1);
 const list = computed(() => {
   const start = (page.value - 1) * pageSize.value;
