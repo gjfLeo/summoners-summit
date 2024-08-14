06b23e605e73e79cec81d9bbdb2e036d24f2f581
guojifeng <guojifeng@189.cn>
2024-08-14 12:32:08 +0800

    fix: 卡牌图片修改

--- utils/images/index.ts
+++ utils/images/index.ts
@@ -2,13 +2,17 @@ import md5 from "crypto-js/md5";
 import { ALL_ACTION_CARDS_INFO, ALL_CHARACTER_CARDS_INFO } from "../cards";
 import type { ActionCard, CharacterCard } from "~/utils/types";
 
+const fandomFilenameOverrides: Record<string, string> = {
+  212111: "Hear Me — Let Us Raise the Chalice of Love! Equipment Card.png",
+};
+
 function getImageUrl(filename: string) {
   filename = filename.replaceAll(":", "");
   filename = filename.replaceAll("?", "");
-  filename = filename.replaceAll("\"", "");
   filename = filename.replaceAll(" ", "_");
   const hash = md5(filename).toString();
   filename = filename.replace("'", "%27");
+  filename = filename.replace("\"", "%22");
   return `https://static.wikia.nocookie.net/gensin-impact/images/${hash.charAt(0)}/${hash.slice(0, 2)}/${filename}`;
 }
 
@@ -17,7 +21,7 @@ export const cardImages = Object.fromEntries(
     ...Object.entries(ALL_CHARACTER_CARDS_INFO)
       .map(([card, { nameEn }]) => [card, getImageUrl(`${nameEn} Character Card.png`)]),
     ...Object.entries(ALL_ACTION_CARDS_INFO)
-      .map(([card, { nameEn, type }]) => [card, getImageUrl(`${nameEn} ${type} Card.png`)]),
+      .map(([card, { nameEn, type }]) => [card, getImageUrl(`${fandomFilenameOverrides[card] || nameEn} ${type} Card.png`)]),
   ],
 ) as Record<CharacterCard | ActionCard, string>;
 
