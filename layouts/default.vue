d85f3c73420e240fcfcd0811d9823f58d7b5d044
gjfLeo <gjfLeo@yeah.net>
2024-08-23 17:49:28 +0800

    feat: 菜单高亮

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,11 +12,17 @@ const routes: SiteRoute[] = [
     label: "tournaments",
     to: "/tournaments",
     // icon: "i-mingcute:medal-line",
+    matches: [
+      /^\/tournament\//,
+    ],
   },
   {
     label: "teams",
     to: "/teams",
     // icon: "i-mingcute:group-3-line",
+    matches: [
+      /^\/team\//,
+    ],
   },
   {
     label: "cards",
@@ -26,6 +32,9 @@ const routes: SiteRoute[] = [
     label: "players",
     to: "/players",
     // icon: "i-mingcute:user-4-line",
+    matches: [
+      /^\/player\//,
+    ],
   },
   {
     label: "tools",
