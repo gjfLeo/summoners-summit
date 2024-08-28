7e580a4a693bd84fe4d31bcd0eb5715a841dc3a9
readice <101871184+readice@users.noreply.github.com>
2024-08-28 17:08:27 +0800

    Update index.ts

--- server/data/tournaments/4.8/index.ts
+++ server/data/tournaments/4.8/index.ts
@@ -1,9 +1,11 @@
 import t41 from "./外服赛事-小王子杯-大西洋赛区-淘汰赛";
 import t42 from "./最终入围赛";
 import t43 from "./外服赛事-小王子杯-太平洋赛区-淘汰赛";
+import t44 from "./枫达杯";
 
 export default [
   t41,
   t42,
   t43,
+  t44,
 ];
