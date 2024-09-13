8434eb18355fd8f099fe318f4f663ba80f358984
gjfLeo <gjfLeo@yeah.net>
2024-09-13 16:58:00 +0800

    feat: 跳转域名

--- composables/use-site-notification.ts
+++ composables/use-site-notification.ts
@@ -1,15 +1,28 @@
+import type { NotificationOptions } from "naive-ui";
 import { NButton } from "#components";
+
+type NotificationData = Record<string, {
+  latestVersion: number;
+  autoRead?: boolean;
+} & Pick<NotificationOptions, "type" | "title" | "content">>;
 
 export function useSiteNotification() {
   const { t } = useLocales();
-  const notificationData = computed(() => ({
+  const notificationData = computed<NotificationData>(() => ({
+    changeDomain: {
+      type: "info",
+      title: t("site.notifications.changeDomain.title"),
+      content: t("site.notifications.changeDomain.content"),
+      latestVersion: 1,
+      autoRead: true,
+    },
     copyDeckCode: {
       type: "success",
       title: t("site.notifications.copyDeckCode.title"),
       content: t("site.notifications.copyDeckCode.content"),
       latestVersion: 1,
     },
-  }));
+  } satisfies NotificationData));
   type NotificationKey = keyof typeof notificationData.value;
 
   const notification = useNotification();
@@ -19,23 +32,33 @@ export function useSiteNotification() {
     const readVersion = notificationRead.value[key] ?? 0;
     if (readVersion >= notificationData.value[key].latestVersion) return;
 
-    const notificationInstance = notification.success({
+    const notificationInstance = notification.create({
+      type: notificationData.value[key].type,
       title: notificationData.value[key].title,
       content: notificationData.value[key].content,
-      action: () => {
-        return h(NButton, {
-          tertiary: true,
-          size: "small",
-          onClick: () => {
-            notificationRead.value[key] = notificationData.value[key].latestVersion;
-            notificationInstance.destroy();
+      action: notificationData.value[key].autoRead
+        ? undefined
+        : () => {
+            return h(NButton, {
+              tertiary: true,
+              size: "small",
+              onClick: () => {
+                notificationRead.value[key] = notificationData.value[key].latestVersion;
+                notificationInstance.destroy();
+              },
+            }, () => t("site.messages.doNotShowAgain"));
           },
-        }, () => t("site.messages.doNotShowAgain"));
-      },
       keepAliveOnHover: true,
       duration: 8000,
     });
+    if (notificationData.value[key].autoRead) {
+      notificationRead.value[key] = notificationData.value[key].latestVersion;
+    }
   }
 
-  return { showNotification };
+  function setNotificationReadVersion(key: NotificationKey, version: number) {
+    notificationRead.value[key] = version;
+  }
+
+  return { showNotification, setNotificationReadVersion };
 }
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,27 @@
 <template>
-  <SiteLayout :routes="routes">
+  <SiteLayout v-if="!needRedirect" :routes="routes">
     <NuxtPage />
   </SiteLayout>
 </template>
 
 <script lang="ts" setup>
 import type { SiteRoute } from "../composables/use-site-menu";
+
+const runtimeConfig = useRuntimeConfig();
+const route = useRoute();
+const needRedirect = runtimeConfig.public.needRedirect;
+const { setNotificationReadVersion, showNotification } = useSiteNotification();
+const urlSearch = useUrlSearchParams();
+if (needRedirect && import.meta.client && !import.meta.dev) {
+  window.location.href = "https://summoners.top?fromOldDomain=1";
+}
+onMounted(() => {
+  if (route.query.fromOldDomain || (import.meta.dev && needRedirect)) {
+    delete urlSearch.fromOldDomain;
+    setNotificationReadVersion("changeDomain", 0);
+    showNotification("changeDomain");
+  }
+});
 
 const routes: SiteRoute[] = [
   {
