785b5422291b4f6434928e5b0624a04e23833992
gjfLeo <gjfLeo@yeah.net>
2024-10-11 17:39:21 +0800

    pref: 对局列表优化

--- server/api/v3/games/list.get.ts
+++ server/api/v3/games/list.get.ts
@@ -12,14 +12,25 @@ export default defineEventHandler(async (event) => {
     games = games.filter(game => game.gameVersion === gameVersion);
   }
 
-  let gameDetails = games.map(fillGameDetail).flatMap(game => [game, getMirroredGameDetail(game)]);
-
   if (teamId) {
-    gameDetails = gameDetails.filter(game => game.playerADeck.teamId === teamId);
+    games = games.filter(game => game.playerADeck.teamId === teamId || game.playerBDeck.teamId === teamId);
   }
   if (deckCode) {
-    gameDetails = gameDetails.filter(game => game.playerADeck.deckCode === deckCode);
+    games = games.filter(game => game.playerADeck.deckCode === deckCode || game.playerBDeck.deckCode === deckCode);
   }
+
+  let gameDetails = games
+    .flatMap((game) => {
+      if ((teamId && game.playerADeck.teamId === teamId)
+        || (deckCode && game.playerADeck.deckCode === deckCode)) {
+        return [fillGameDetail(game)];
+      }
+      if ((teamId && game.playerBDeck.teamId === teamId)
+        || (deckCode && game.playerBDeck.deckCode === deckCode)) {
+        return [getMirroredGameDetail(fillGameDetail(game))];
+      }
+      return [fillGameDetail(game)];
+    });
 
   gameDetails = gameDetails.slice(0, 30);
 
