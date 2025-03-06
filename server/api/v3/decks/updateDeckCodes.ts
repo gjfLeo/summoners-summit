d85d5e244a9eee9d0e3774137ef9b055ee115f84
gjfLeo <gjfLeo@yeah.net>
2025-03-06 09:17:08 +0800

    feat: 重新生成牌组分享码功能

---
+++ server/api/v3/decks/updateDeckCodes.ts
@@ -0,0 +1,20 @@
+import { decodeDeck, encodeDeck, getGameList, saveGame } from "~/server/service";
+import { blockWords } from "~/utils/deck";
+
+export default defineEventHandler(async () => {
+  const games = getGameList();
+  for (const game of games) {
+    let changed = false;
+    for (const deck of [game.playerADeck, game.playerBDeck]) {
+      const deckCode = deck.deckCode;
+      if (deckCode && blockWords.some(word => deckCode.includes(word))) {
+        deck.deckCode = encodeDeck(decodeDeck(deckCode));
+        changed = true;
+      }
+    }
+    if (changed) {
+      saveGame(game);
+    }
+  }
+  return responseOk();
+});
