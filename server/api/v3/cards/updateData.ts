c81d2284c631e0828bc0b867405005cdc891a10e
Liger <gjfleo@yeah.net>
2025-03-08 14:38:02 +0800

    feat: 更新5.4卡牌

--- server/api/v3/cards/updateData.ts
+++ server/api/v3/cards/updateData.ts
@@ -7,7 +7,7 @@ const fandomFilenameOverrides: Record<number, string> = {
   212111: "Hear Me — Let Us Raise the Chalice of Love! Equipment Card.png",
   321024: "Scions of the Canopy Support Card.png",
   321025: "People of the Springs Support Card.png",
-  321026: "Flower-Feather Clan.png",
+  321026: "Flower-Feather Clan Support Card.png",
 };
 
 function getFandomImageUrl(filename: string) {
