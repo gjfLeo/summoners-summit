e885b48d926f7de7e9b82fc62488e001364abbc4
gjfLeo <gjfLeo@yeah.net>
2025-05-12 10:00:27 +0800

    fix: 尝试修复#116

--- middleware/game-version-navigator.ts
+++ middleware/game-version-navigator.ts
@@ -2,10 +2,16 @@ export default defineNuxtRouteMiddleware((to, from) => {
   if (import.meta.client) {
     const { gameVersionPath } = useGameVersion({ skipDetection: true });
     if (to.name === from.name) {
-      return navigateTo(`${location.href}/${gameVersionPath.value}`, { replace: true, open: { target: "_self" } });
+      return navigateTo(
+        `${location.href}/${gameVersionPath.value}`,
+        { replace: true, open: { target: "_self" }, external: true },
+      );
     }
     else {
-      return navigateTo(`${to.path}/${gameVersionPath.value}`, { replace: true });
+      return navigateTo(
+        `${to.path}/${gameVersionPath.value}`,
+        { replace: true },
+      );
     }
   }
 });
