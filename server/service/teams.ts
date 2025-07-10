7f73493dcd060920598ab3742ca79624d2b6f17c
gjfLeo <gjfLeo@yeah.net>
2025-07-10 17:28:21 +0800

    feat: 阵容牌组API函数缓存

--- server/service/teams.ts
+++ server/service/teams.ts
@@ -131,70 +131,78 @@ export function getTeamMatchupStats(params: GetAllTeamMatchupsParams) {
 /**
  * 计算胜率时，平局视为负
  */
-export async function getTeamDecksStats({ gameVersion, teamId }: {
-  teamId: DeckTeamId;
-  gameVersion?: GameVersionId;
-}) {
-  let games = await getStorageGameList();
-  if (gameVersion) {
-    games = games.filter(g => g.gameVersion === gameVersion);
-  }
+export const getTeamDecksStats = defineCachedFunction(
+  async ({ gameVersion, teamId }: {
+    teamId: DeckTeamId;
+    gameVersion?: GameVersionId;
+  }) => {
+    let games = await getStorageGameList();
+    if (gameVersion) {
+      games = games.filter(g => g.gameVersion === gameVersion);
+    }
 
-  const decks: { deckCode: DeckCode; win: boolean }[] = [];
-  games.forEach((game) => {
-    for (const p of ["A", "B"] as const) {
-      const deck = game[`player${p}Deck`];
-      if (deck.deckCode && deck.teamId === teamId) {
-        decks.push({
-          deckCode: deck.deckCode,
-          win: game.winner === p,
-        });
+    const decks: { deckCode: DeckCode; win: boolean }[] = [];
+    games.forEach((game) => {
+      for (const p of ["A", "B"] as const) {
+        const deck = game[`player${p}Deck`];
+        if (deck.deckCode && deck.teamId === teamId) {
+          decks.push({
+            deckCode: deck.deckCode,
+            win: game.winner === p,
+          });
+        }
       }
-    }
-  });
-
-  const decksRecord: Record<DeckCode, {
-    deckCode: DeckCode;
-    numGames: number;
-    numGamesWin: number;
-    cardCountRecord: Record<CardId, number>;
-  }> = {};
-  decks.forEach((deck) => {
-    const recordItem = decksRecord[deck.deckCode] ??= {
-      deckCode: deck.deckCode,
-      numGames: 0,
-      numGamesWin: 0,
-      cardCountRecord: getActionCardCountRecord(deck.deckCode),
-    };
-    recordItem.numGames++;
-    if (deck.win) {
-      recordItem.numGamesWin++;
-    }
-  });
-
-  // 胜利对局的额外加权，0表示不考虑
-  const winWeight = 1;
-  const maxScorePoint = decks.length + decks.filter(d => d.win).length * winWeight;
-  // 卡牌的分数，0 ~ 2
-  const cardScoreRecord: Record<CardId, number> = {};
-  Object.values(decksRecord).forEach((deck) => {
-    const cardCountRecord = deck.cardCountRecord;
-    Object.entries(cardCountRecord)
-      .forEach(([cardId, count]) => {
-        cardScoreRecord[cardId] ??= 0;
-        cardScoreRecord[cardId] += count * (deck.numGames + (deck.numGamesWin * winWeight)) / maxScorePoint;
-      });
-  });
-
-  return Object.values(decksRecord)
-    .map((deck) => {
-      return {
-        deckCode: deck.deckCode as DeckCode,
-        numGames: deck.numGames,
-        numGamesWin: deck.numGamesWin,
-        distanceToAverage: Object.entries(cardScoreRecord)
-          .map(([cardId, score]) => Math.abs(score - (deck.cardCountRecord[cardId] ?? 0)))
-          .reduce((acc, cur) => acc + cur, 0),
-      };
     });
-}
+
+    const decksRecord: Record<DeckCode, {
+      deckCode: DeckCode;
+      numGames: number;
+      numGamesWin: number;
+      cardCountRecord: Record<CardId, number>;
+    }> = {};
+    decks.forEach((deck) => {
+      const recordItem = decksRecord[deck.deckCode] ??= {
+        deckCode: deck.deckCode,
+        numGames: 0,
+        numGamesWin: 0,
+        cardCountRecord: getActionCardCountRecord(deck.deckCode),
+      };
+      recordItem.numGames++;
+      if (deck.win) {
+        recordItem.numGamesWin++;
+      }
+    });
+
+    // 胜利对局的额外加权，0表示不考虑
+    const winWeight = 1;
+    const maxScorePoint = decks.length + decks.filter(d => d.win).length * winWeight;
+    // 卡牌的分数，0 ~ 2
+    const cardScoreRecord: Record<CardId, number> = {};
+    Object.values(decksRecord).forEach((deck) => {
+      const cardCountRecord = deck.cardCountRecord;
+      Object.entries(cardCountRecord)
+        .forEach(([cardId, count]) => {
+          cardScoreRecord[cardId] ??= 0;
+          cardScoreRecord[cardId] += count * (deck.numGames + (deck.numGamesWin * winWeight)) / maxScorePoint;
+        });
+    });
+
+    return Object.values(decksRecord)
+      .map((deck) => {
+        return {
+          deckCode: deck.deckCode as DeckCode,
+          numGames: deck.numGames,
+          numGamesWin: deck.numGamesWin,
+          distanceToAverage: Object.entries(cardScoreRecord)
+            .map(([cardId, score]) => Math.abs(score - (deck.cardCountRecord[cardId] ?? 0)))
+            .reduce((acc, cur) => acc + cur, 0),
+        };
+      });
+  },
+  {
+    maxAge: import.meta.dev ? 1 : 0,
+    name: "getTeamDecksStats",
+    getKey: ({ teamId, gameVersion }) =>
+      [teamId, gameVersion].filter(Boolean).join(":"),
+  },
+);
