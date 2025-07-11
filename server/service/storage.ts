6319d67ef5dba4c3d43fd40a5c70c52ae0a16bbc
gjfLeo <gjfLeo@yeah.net>
2025-07-11 18:38:15 +0800

    feat: storage缓存并行优化

--- server/service/storage.ts
+++ server/service/storage.ts
@@ -9,13 +9,14 @@ export function defineGetRecordStorage<K extends string, V>(
     async (): Promise<Record<K, V>> => {
       console.log(`Reading Storage: ${path}`);
       const storage = useStorage(`assets:data:${path}`);
-      const keys = (await storage.getKeys()) as K[];
+      const keys = await storage.getKeys();
       const record: Partial<Record<K, V>> = {};
       await runParallel(
         new Set(keys),
-        async (key: K) => {
+        async (key) => {
           const item = await storage.getItem(key);
-          record[key] = zodType.parse(item);
+          const id = key.split(".")[0] as K;
+          record[id] = zodType.parse(item);
         },
         { concurrency: 10 },
       );
